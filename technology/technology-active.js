73ed068dcbba4b4befc2758e781f0ea6c6ddc833
Matth-s <108790362+Matth-s@users.noreply.github.com>
2022-08-07 18:35:15 +0200

    Update technology-active.js

--- technology/technology-active.js
+++ technology/technology-active.js
@@ -16,7 +16,7 @@ menu.forEach((item) => {
             case "un" :
                 titre.innerHTML = "LAUNCH VEHICULE"
                 paragraphe.innerHTML = "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
-                image.src ="/assets/technology/image-launch-vehicle-portrait.jpg"
+                image.src ='../assets/technology/image-launch-vehicle-portrait.jpg'
                 menu[0].classList.add('rond-click');
                 menu[1].classList.remove('rond-click');
                 menu[2].classList.remove('rond-click');
@@ -28,7 +28,7 @@ menu.forEach((item) => {
             case "deux":
                 titre.innerHTML = "SPACEPORT"
                 paragraphe.innerHTML = "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch"
-                image.src ="/assets/technology/image-spaceport-portrait.jpg"
+                image.src ='../assets/technology/image-spaceport-portrait.jpg'
                 menu[0].classList.remove('rond-click');
                 menu[1].classList.add('rond-click');
                 menu[2].classList.remove('rond-click');
@@ -40,7 +40,7 @@ menu.forEach((item) => {
             case "trois":
                 titre.innerHTML = "SPACE CAPSULE"
                 paragraphe.innerHTML = "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
-                image.src ="/assets/technology/image-space-capsule-portrait.jpg"
+                image.src ='../assets/technology/image-space-capsule-portrait.jpg'
                 menu[0].classList.remove('rond-click');
                 menu[1].classList.remove('rond-click');
                 menu[2].classList.add('rond-click');
@@ -60,7 +60,7 @@ chiffre.forEach((item) => {
             case "un-span" :
                 titre.innerHTML = "LAUNCH VEHICULE"
                 paragraphe.innerHTML = "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
-                image.src ="/assets/technology/image-launch-vehicle-portrait.jpg"
+                image.src ='../assets/technology/image-launch-vehicle-portrait.jpg'
                 menu[0].classList.add('rond-click');
                 menu[1].classList.remove('rond-click');
                 menu[2].classList.remove('rond-click');
@@ -72,7 +72,7 @@ chiffre.forEach((item) => {
             case "deux-span":
                 titre.innerHTML = "SPACEPORT"
                 paragraphe.innerHTML = "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch"
-                image.src ="/assets/technology/image-spaceport-portrait.jpg"
+                image.src ='../assets/technology/image-spaceport-portrait.jpg'
                 
                 menu[0].classList.remove('rond-click');
                 menu[1].classList.add('rond-click');
@@ -85,7 +85,7 @@ chiffre.forEach((item) => {
             case "trois-span":
                 titre.innerHTML = "SPACE CAPSULE"
                 paragraphe.innerHTML = "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
-                image.src ="/assets/technology/image-space-capsule-portrait.jpg"
+                image.src ='../assets/technology/image-space-capsule-portrait.jpg'
                 menu[0].classList.remove('rond-click');
                 menu[1].classList.remove('rond-click');
                 menu[2].classList.add('rond-click');
