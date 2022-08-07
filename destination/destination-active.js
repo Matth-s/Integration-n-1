37e35a417aca96e9c72b81d8ba1b4bd63b47d18a
Matth-s <108790362+Matth-s@users.noreply.github.com>
2022-08-07 18:30:25 +0200

    Update destination-active.js

--- destination/destination-active.js
+++ destination/destination-active.js
@@ -10,7 +10,7 @@ liste_planete.forEach((item) => {
     item.addEventListener('click', function(e) {
         switch(e.target.id) {
             case "moon" :
-                image.src = "/assets/destination/image-moon.png"
+                image.src = '../assets/destination/image-moon.png'
                 titre_planete.innerHTML = "MOON"
                 paragraphe.innerHTML = "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites."
                 distance.innerHTML = "384,700 KM"
@@ -21,7 +21,7 @@ liste_planete.forEach((item) => {
                 liste_planete[3].classList.remove('toggle')
                 break;
             case "mars" :
-                image.src = "/assets/destination/image-mars.png"
+                image.src = '../assets/destination/image-mars.png'
                 titre_planete.innerHTML = "Mars"
                 paragraphe.innerHTML = "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!"
                 distance.innerHTML = "255 MIL KM"
@@ -32,7 +32,7 @@ liste_planete.forEach((item) => {
                 liste_planete[3].classList.remove('toggle')
                 break;
             case "europa" :
-                image.src = "/assets/destination/image-europa.png"
+                image.src = '../assets/destination/image-europa.png'
                 titre_planete.innerHTML = "EUROPA"
                 paragraphe.innerHTML = "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin."
                 distance.innerHTML = "628 MIL KM"
@@ -43,7 +43,7 @@ liste_planete.forEach((item) => {
                 liste_planete[3].classList.remove('toggle')
                 break;
             case "titan" :
-                image.src = "/assets/destination/image-titan.png"
+                image.src = '../assets/destination/image-titan.png'
                 titre_planete.innerHTML = "TITAN"
                 paragraphe.innerHTML = "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn"
                 distance.innerHTML = "1.6 BIL KM"
