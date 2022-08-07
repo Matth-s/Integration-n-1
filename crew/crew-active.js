6d51c6f61053475ecb7c1b435d03091979a35897
Matth-s <108790362+Matth-s@users.noreply.github.com>
2022-08-07 18:21:49 +0200

    Update crew-active.js

--- crew/crew-active.js
+++ crew/crew-active.js
@@ -14,7 +14,7 @@ rond.forEach((item) => {
             grade.innerHTML ="COMMANDER"
             personnage.innerHTML ="DOUGLAS HURLEY"
             paragraphe.innerHTML ="Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2."
-            image.src ="/assets/crew/image-douglas-hurley.png"
+            image.src ='../assets/crew/image-douglas-hurley.png'
             rond[0].classList.add('rond-click');
             rond[1].classList.remove('rond-click');
             rond[2].classList.remove('rond-click');
@@ -24,7 +24,7 @@ rond.forEach((item) => {
             grade.innerHTML ="MISSION SPECIALIST"
             personnage.innerHTML ="MARK SHUTTLEWORTH"
             paragraphe.innerHTML ="Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space touris"
-            image.src ="/assets/crew/image-mark-shuttleworth.png"
+            image.src ='..assets/crew/image-mark-shuttleworth.png'
             rond[0].classList.remove('rond-click');
             rond[1].classList.add('rond-click');
             rond[2].classList.remove('rond-click');
@@ -34,7 +34,7 @@ rond.forEach((item) => {
             grade.innerHTML ="PILOT"
             personnage.innerHTML ="VICTOR GLOVER"
             paragraphe.innerHTML ="Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. "
-            image.src ="/assets/crew/image-victor-glover.png"
+            image.src ='../assets/crew/image-victor-glover.png'
             rond[0].classList.remove('rond-click');
             rond[1].classList.remove('rond-click');
             rond[2].classList.add('rond-click');
@@ -44,7 +44,7 @@ rond.forEach((item) => {
             grade.innerHTML ="FLIGHT ENGINEER"
             personnage.innerHTML ="ANOUSHEH ANSARI"
             paragraphe.innerHTML ="Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space. "
-            image.src ="/assets/crew/image-anousheh-ansari.png"
+            image.src ='../assets/crew/image-anousheh-ansari.png'
             rond[0].classList.remove('rond-click');
             rond[1].classList.remove('rond-click');
             rond[2].classList.remove('rond-click');
